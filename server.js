096b1ffac1cbc21adb29fa636f29165db9579bb6
Diolindo Luis <luis.diolindo@gmail.com>
2022-06-07 18:16:05 +0200

    Updating response

--- server.js
+++ server.js
@@ -51,6 +51,7 @@ app.post('/register', (req, res) => {
         id: '125',
         name: name,
         email: email,
+        password: password,
         entries: 0,
         joined: new Date()
     });
@@ -72,7 +73,7 @@ app.get('/profile/:id', (req, res) => {
     }
 });
 
-app.post('/image', (req, res) => {
+app.put('/image', (req, res) => {
     const { id } = req.body;
     let found = false;
     database.users.forEach(user => {
